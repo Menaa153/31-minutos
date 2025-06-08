b42faa2a1c6bfc57e7318e61a7b6d46d257618d5
francisco-micolta <70337426+francisco-micolta@users.noreply.github.com>
2025-06-08 05:28:07 +0000

    deportes

--- src/pages/deportes.jsx
+++ src/pages/deportes.jsx
@@ -1,8 +1,51 @@
-export default function Deportes() {
+import { useEffect, useState, useRef } from 'react';
+import '../css/deportes.css'; 
+
+const Deportes = () => {
   return (
     <div>
-      <h1>Deportes</h1>
-      <p>Las hazañas más extravagantes del mundo deportivo.</p>
+      <div className="deportes-banner">
+        <h2>Deportes</h2>
+        <p>Las últimas novedades sobre los deportistas más extravagantes.</p>
+        <button>Ver noticias</button>
+      </div>
+
+      <div className="noticias-container">
+        <h3>Últimas Noticias sobre Deportes</h3>
+        <div className="noticias-grid">
+          <div className="noticia-card">
+            <span className="categoria">Categoría</span>
+            <h3>Título</h3>
+            <p>Fecha de publicación</p>
+            <p>Descripción</p>
+            <div className="autor">
+              <span>◯</span>
+              <span>Autor</span>
+            </div>
+            <button>Leer más</button>
+          </div>
+
+          <div className="noticia-card">
+            <span className="categoria">Categoría</span>
+            <h3>Título</h3>
+            <p>Fecha de publicación</p>
+            <p>Descripción</p>
+            <div className="autor">
+              <span>◯</span>
+              <span>Autor</span>
+            </div>
+            <button>Leer más</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="hazaña-banner">
+        <h3>La hazaña de la semana</h3>
+      </div>
     </div>
   );
-}
+};
+
+export default Deportes;
+
+
--- src/pages/horoscopo.jsx
+++ src/pages/horoscopo.jsx
@@ -18,7 +18,7 @@ export default function Horoscopo() {
     const obtenerHoroscopo = async () => {
       try {
         const response = await fetch(
-          `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
+         `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
           {
             method: 'POST',
             headers: {
